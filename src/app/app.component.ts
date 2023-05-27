80d990a64cf7f30e4320df6a46770d00098aeb0d
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-27 13:57:48 -0400

    changed background color and text color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaystationService } from './services/playstation.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  data: any[] = [];
+
+  constructor(private playstationService: PlaystationService) {}
+
+  ngOnInit() {
+    this.fetchPlaystationData();
+  }
+
   title = 'my-first-angular';
+
+  fetchPlaystationData() {
+    this.playstationService.getPlaystationData().subscribe(
+      (data) => {
+        console.log(data); // Display the data in the console
+        this.data = data;// Handle the data as per your requirements
+      },
+      (error) => {
+        console.error('An error occurred:', error);
+        // Handle the error appropriately
+      }
+    );
+  }
 }
